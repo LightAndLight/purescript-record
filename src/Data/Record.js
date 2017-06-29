3db2a215f334a82a1a67664e466709f92128cb81
Isaac Elliott <isaace71295@gmail.com>
2017-06-29 14:56:35 +1000

    Initial commit

---
+++ src/Data/Record.js
@@ -0,0 +1,37 @@
+"use strict";
+
+exports.merge = function (dict) {
+    return function (l) {
+        return function (r) {
+            return Object.assign({}, r, l);
+        };
+    };
+};
+
+exports.restrict = function(isSymbolDict) {
+    return function(rowConsDict) {
+        return function(proxy) {
+            return function(obj) {
+                var newObj = Object.assign({}, obj);
+                delete newObj[isSymbolDict.reflectSymbol(proxy)];
+                return newObj;
+            };
+        };
+    };
+};
+
+exports.extend = function(isSymbolDict) {
+    return function(rowConsDict) {
+        return function(proxy) {
+            return function(value) {
+                return function(obj) {
+                    var newObj = {};
+                    newObj[isSymbolDict.reflectSymbol(proxy)] = value;
+
+                    return Object.assign({}, obj, newObj);
+                };
+            };
+        };
+    };
+};
+
---
+++ src/Data/Record.purs
@@ -0,0 +1,30 @@
+module Data.Record where
+
+import Data.Symbol (class IsSymbol, SProxy)
+
+-- | Left-biased record merge
+foreign import merge
+  :: forall l r result
+   . Union l r result
+  => Record l
+  -> Record r
+  -> Record result
+
+-- | Record restriction
+foreign import restrict
+  :: forall label a tail result
+   . IsSymbol label
+  => RowCons label a tail result
+  => SProxy label
+  -> Record tail
+  -> Record result
+
+-- | Record extension
+foreign import extend
+  :: forall label a tail result
+   . IsSymbol label
+  => RowCons label a tail result
+  => SProxy label
+  -> a
+  -> Record tail
+  -> Record result
